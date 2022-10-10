1aa56eca4aac96a2086cedfe41301be7eabb84e3
Андрей Гришков <andreygorm@gmail.com>
2022-10-10 19:32:52 +0300

    test

--- testMe.js
+++ testMe.js
@@ -3,3 +3,5 @@
 // ssh-copy-id -i ~/.ssh/id_rsa_practicum.pub andrey_grishkov@51.250.11.125
 // ssh-copy-id -i C:/Users/ACER/.ssh/id_rsa_practicum.pub andrey_grishkov@51.250.11.125
 // ssh-copy-id -i C:\Users\ACER\.ssh\id_rsa_practicum.pub andrey_grishkov@51.250.11.125
+// test 10
+console.log('hello Kitty');
