02fc1aafa0cca5063f0929cc6317d9443ef8eb9b
Андрей Гришков <andreygorm@gmail.com>
2022-10-13 10:34:02 +0300

    refactoring cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,27 +4,27 @@ const allowedCors = [
   'localhost:3000',
   'http://localhost:3000',
   'https://localhost:3000',
-  'http://mesto.front.grishkov.nomoredomains.icu/',
-  'https://mesto.front.grishkov.nomoredomains.icu/'
+  'http://mesto.front.grishkov.nomoredomains.icu',
+  'https://mesto.front.grishkov.nomoredomains.icu'
 ];
 
-module.exports = (req, res, next) => {
+const cors = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Credentials', true);
-  res.set("Content-Type", "application/json");
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
-
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
 
-  next();
-  return null;
+  return next();
 };
+
+module.exports = cors;
